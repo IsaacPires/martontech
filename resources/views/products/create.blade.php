203868cf64b5db13840eaf80d199cf19a49813d3
Raghi Rashid <eduardo.raghi@hotmail.com>
2024-04-11 16:49:11 -0300

    correcao dos dados do formulario

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -4,15 +4,15 @@
         <div class="row">
             <div class="col-md-4">
                 <label for="Name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="Name" name="Name" required>
+                <input type="text" class="form-control" id="Name" name="Name" minlength="5" required>
             </div>
             <div class="col-md-4">
                 <label for="AlertQuantity" class="form-label">Quantidade em Alerta</label>
-                <input type="text" class="form-control" id="AlertQuantity" name="AlertQuantity" required>
+                <input type="number" class="form-control" id="AlertQuantity" name="AlertQuantity" required>
             </div>
             <div class="col-md-4">
                 <label for="StockQuantity" class="form-label">Quatidade em Estoque</label>
-                <input type="text" class="form-control" id="StockQuantity" name="StockQuantity" required>
+                <input type="number" class="form-control" id="StockQuantity" name="StockQuantity" required>
             </div>
             <div class="col-md-4">
                 <label for="suppliers_id" class="form-label">Fornecedor</label>
--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -5,15 +5,15 @@
         <div class="row">
             <div class="col-md-4">
                 <label for="Name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="Name" name="Name" value='{{$products->Name}}' required>
+                <input type="text" class="form-control" id="Name" name="Name" value='{{$products->Name}}' minlength="5" required>
             </div>
             <div class="col-md-4">
                 <label for="AlertQuantity" class="form-label">Quantidade em Alerta</label>
-                <input type="text" class="form-control" id="AlertQuantity" name="AlertQuantity" value='{{$products->AlertQuantity}}' required>
+                <input type="number" class="form-control" id="AlertQuantity" name="AlertQuantity" value='{{$products->AlertQuantity}}' required>
             </div>
             <div class="col-md-4">
                 <label for="StockQuantity" class="form-label">Quatidade em Estoque</label>
-                <input type="text" class="form-control" id="StockQuantity" name="StockQuantity" value='{{$products->StockQuantity}}' required>
+                <input type="number" class="form-control" id="StockQuantity" name="StockQuantity" value='{{$products->StockQuantity}}' required>
             </div>
             <div class="col-md-4">
                 <label for="suppliers_id" class="form-label">Fornecedor</label>
