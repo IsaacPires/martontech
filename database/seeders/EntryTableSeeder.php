8cd8c4087f021e079a1c9dade6174e220974843e
Isaac Pires <isaac.alves.1005@gmail.com>
2024-06-16 21:41:05 -0300

    teste seed

--- database/seeders/EntryTableSeeder.php
+++ database/seeders/EntryTableSeeder.php
@@ -18,25 +18,20 @@ class EntryTableSeeder extends Seeder
     {
         $path = storage_path('app/dbFiles/entry_products.json');
 
-        if (!file_exists($path))
-        {
-            return;
-        }
+        $datas = json_decode(file_get_contents($path), true);
 
-        $entryProducts = json_decode(file_get_contents($path), true);
-
-        foreach ($entryProducts as $key => $entryProduct)
+        foreach ($datas as $data)
         {
             EntryProducts::create([
-                'products_id'       => array_key_exists('Products_name', $entryProduct) ? Products::where('Name', 'like', "%{$entryProduct['Products_name']}%")->value('id') : NULL,
-                'SellerName'        => array_key_exists('SellerName', $entryProduct) ? $entryProduct['SellerName'] : NULL,
-                'Suppliers_id'      => array_key_exists('Suppliers_name', $entryProduct) ? Suppliers::where('Name', 'like', "%{$entryProduct['Suppliers_name']}%")->value('id') : NULL,
-                'Brand'             => array_key_exists('Brand', $entryProduct) ? $entryProduct['Brand'] : NULL,
-                'UnitPrice'         => array_key_exists('UnitPrice', $entryProduct) && is_numeric($entryProduct['UnitPrice']) ? $entryProduct['UnitPrice'] : 0.00,
-                "WithdrawalAmount"  => array_key_exists('WithdrawalAmount.', $entryProduct) ? $entryProduct['WithdrawalAmount.'] : NULL,
-                'TotalPrice'        => array_key_exists('TotalPrice', $entryProduct) && is_numeric($entryProduct['TotalPrice']) ? $entryProduct['TotalPrice'] : 0.00,
-                'created_at'        => array_key_exists('Created_at', $entryProduct) ? date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $entryProduct['Created_at']))) : date('Y-m-d H:i:s'),
-                'updated_at'        => array_key_exists('Created_at', $entryProduct) ? date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $entryProduct['Created_at']))) : date('Y-m-d H:i:s')
+                'products_id'       => array_key_exists('Products_name', $data) ? Products::where('Name', 'like', "%{$data['Products_name']}%")->value('id') : NULL,
+                'SellerName'        => array_key_exists('SellerName', $data) ? $data['SellerName'] : NULL,
+                'Suppliers_id'      => array_key_exists('Suppliers_name', $data) ? Suppliers::where('Name', 'like', "%{$data['Suppliers_name']}%")->value('id') : NULL,
+                'Brand'             => array_key_exists('Brand', $data) ? $data['Brand'] : NULL,
+                'UnitPrice'         => array_key_exists('UnitPrice', $data) && is_numeric($data['UnitPrice']) ? $data['UnitPrice'] : 0.00,
+                "WithdrawalAmount"  => array_key_exists('WithdrawalAmount.', $data) ? $data['WithdrawalAmount.'] : NULL,
+                'TotalPrice'        => array_key_exists('TotalPrice', $data) && is_numeric($data['TotalPrice']) ? $data['TotalPrice'] : 0.00,
+                'created_at'        => array_key_exists('Created_at', $data) ? date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $data['Created_at']))) : date('Y-m-d H:i:s'),
+                'updated_at'        => array_key_exists('Created_at', $data) ? date('Y-m-d H:i:s', strtotime(str_replace('/', '-', $data['Created_at']))) : date('Y-m-d H:i:s')
             ]);
         }
     }
