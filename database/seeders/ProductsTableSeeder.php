cc1870a03ad4a903bbc8a7c5f2de236be987b307
Isaac <isaac.pires.dev@gmail.com>
2024-10-12 21:49:33 -0300

    ajuste seed

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -27,12 +27,12 @@ class ProductsTableSeeder extends Seeder
         foreach ($products as $key => $product)
         {
             Products::create([
+                'id' => array_key_exists('id', $product) ? $product['id'] : 'NI',
                 'Name' => array_key_exists('Name', $product) ? $product['Name'] : 'NI',
                 'AlertQuantity' => array_key_exists('AlertQuantity', $product) ? $product['AlertQuantity'] : 0,
                 'StockQuantity' => array_key_exists('StockQuantity', $product) ? $product['StockQuantity'] : 0,
                 'primary_suppliers_id' => array_key_exists('primary_suppliers_id', $product) && $product['primary_suppliers_id']    != 'NULL' ? $product['primary_suppliers_id'] : NULL,
                 'secondary_supplier_id' => array_key_exists('secondary_supplier_id', $product) && $product['secondary_supplier_id'] != 'NULL' ? $product['secondary_supplier_id'] : NULL,
-               
             ]);
         }
     }
